d30f11c324701cd2415eaa356963c5b2bdc66092
Mohamed Kamaleldin <mohammed.kamaleldin@innosoft.com>
2025-11-09 11:33:12 +0300

    Assignment completed

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -2,10 +2,19 @@ import express from "express";
 import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 
-{/*write code for cors*/}
 
+const PORT = 3000;
+const app = express();
+  app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+app.use(cors());
 
-{/*write code to define routes*/}
+ app.get("/", (req, res) => {
+      res.send("Welcome to the Quote Generator API");
+    });
 
-
-{/*write code to create server*/}
+app.get("/api/quote", (req, res) => {
+    const quote = getRandomQuote();
+    res.json({quote})
+});
